51a464bfe6726cf8e66168101a873753a4f093ec
Arthurregismais <Arthur@DESKTOP-PRVT7GH>
2024-07-19 14:21:58 -0300

    Conhecendo o objeto Calendar

--- modulo_datas/src/modulo_datas/DatasEmJava.java
+++ modulo_datas/src/modulo_datas/DatasEmJava.java
@@ -2,6 +2,7 @@ package modulo_datas;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class DatasEmJava {
@@ -10,6 +11,8 @@ public class DatasEmJava {
 
 	public static void main(String[] args) throws ParseException {
 	
+		Calendar calendar = Calendar.getInstance();
+		
 		String diaSemana = null;
 		
 		Date date = new Date();
@@ -50,18 +53,25 @@ public class DatasEmJava {
 		
 		
 		System.out.println("Data em milisegundos: " + date.getTime());
+		System.out.println("Calendar em milisegundos: " + calendar.getTimeInMillis());
 		
 		System.out.println("Dia do mês: " + date.getDate());
+		System.out.println("Calendar do mês: " + calendar.get(Calendar.DAY_OF_MONTH));
 		
 		System.out.println("Dia da semana: " + diaSemana);
+		System.out.println("Calendar da semana: " + calendar.get((Calendar.DAY_OF_WEEK) - 1));
 		
 		System.out.println("Hora do dia: " + date.getHours());
+		System.out.println("Calendar hora do dia: " + calendar.get(Calendar.HOUR_OF_DAY));
 		
 		System.out.println("Minutos da hora: " + date.getMinutes());
+		System.out.println("Calendar minutos do dia: " + calendar.get(Calendar.MINUTE));
 		
 		System.out.println("Segundos: " + date.getSeconds());
+		System.out.println("Calendar segundos: " + calendar.get(Calendar.SECOND));
 		
-		System.out.println("Ano " + (date.getYear() + 1900));
+		System.out.println("Ano: " + (date.getYear() + 1900));
+		System.out.println("Calendar ano: " + calendar.get(Calendar.YEAR) );
 		
 		
 		/*------------------------ SimpleDateFormat ------------------------- */
@@ -69,9 +79,13 @@ public class DatasEmJava {
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm.ss");
 		System.out.println("Data atual em formato padrão e String " + simpleDateFormat.format(date));
 		
+		System.out.println("Calendar Data atual em formato padrão e String " + simpleDateFormat.format(calendar.getInstance().getTime()));
+		
 		
 		simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm.ss");
 		System.out.println("Data em formato de banco de dados: " + simpleDateFormat.format(date));
+		
+		System.out.println("Data em formato de banco de dados: " + simpleDateFormat.format(calendar.getInstance().getTime()));
 		
 		simpleDateFormat = new SimpleDateFormat("yyyy-mm-dd");
 		System.out.println("Objeto Date: " + simpleDateFormat.parse("1987-10-18"));
